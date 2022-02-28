75076d5b95cfc473664436b9965c48f4a91e3734
Adam Bradley <weenie.walker.games@gmail.com>
2022-02-28 12:18:57 -0500

    Finished lesson 6

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+# Comment sign
+
+x_position = 10
+
+# concatenate with a numerical value using str() to convert to string
+print("position = " + str(x_position))
+
+print(type(x_position))
