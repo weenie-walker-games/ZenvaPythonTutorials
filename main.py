2439e7bfa3f209e85f6d0ccd7d61367b7228e1e2
Adam Bradley <weenie.walker.games@gmail.com>
2022-04-29 14:46:24 -0400

    After Lesson 8

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 # Comment sign
 
-x_position = 10
+is_game_over = True
+print(is_game_over)
+print(type(is_game_over))
 
-# concatenate with a numerical value using str() to convert to string
-print("position = " + str(x_position))
+is_game_over = 0
+print(is_game_over)
+print(type(is_game_over))
 
-print(type(x_position))
+string_type = "string"
+print(string_type)
+print(type(string_type))
+
+bool_example = True
+age = 5
+name_and_age = "Bob: {} {}".format(age, bool_example)
+print(name_and_age)
+
+age = 15
+name_and_age = "Bob: {}".format(age)
+print(name_and_age)
